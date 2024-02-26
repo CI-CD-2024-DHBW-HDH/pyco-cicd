021ee0134a5726978270a76ffac4bc84b940e8ab
Daniel Schwager <dschwager@exone.de>
2024-02-26 10:41:32 +0100

    Added easy logic

--- src/logic/bots/easy.ts
+++ src/logic/bots/easy.ts
@@ -1,7 +1,11 @@
 import { Field } from "../game";
+import { randomMove, winningMove } from "./bot";
 
 // the easy bot plays a winning move, if it can
 // otherwise, it plays a random move
 export function easyMove(board: Field[], own: Field): number {
-  return -1
+  if (winningMove(board, own) !== -1){
+    return winningMove(board, own);
+  }
+  return randomMove(Math.random());
 }
