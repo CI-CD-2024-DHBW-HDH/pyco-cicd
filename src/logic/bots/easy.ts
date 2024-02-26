c263b1537f223e557db5d3699bd758214163fa35
Daniel Schwager <dschwager@exone.de>
2024-02-26 11:10:45 +0100

    chaged fixed number to blanks.length

--- src/logic/bots/easy.ts
+++ src/logic/bots/easy.ts
@@ -1,11 +1,13 @@
-import { Field } from "../game";
+import { Field, getBlanks } from "../game";
 import { randomMove, winningMove } from "./bot";
 
 // the easy bot plays a winning move, if it can
 // otherwise, it plays a random move
 export function easyMove(board: Field[], own: Field): number {
+  const blanks = getBlanks(board);
+
   if (winningMove(board, own) !== -1){
     return winningMove(board, own);
   }
-  return randomMove(9);
+  return randomMove(blanks.length);
 }
