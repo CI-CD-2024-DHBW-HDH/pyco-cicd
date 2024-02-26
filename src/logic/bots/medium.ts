2e45cfe01892e425e8c2d2b01778bfd9e5d0b961
Jonas Wiedenmann <jwiedenmann@exone.de>
2024-02-26 10:35:16 +0100

    implemented mediumMove

--- src/logic/bots/medium.ts
+++ src/logic/bots/medium.ts
@@ -1,12 +1,32 @@
-import { getBlanks, invertPlayer, type Field } from "../game";
-import { randomMove, winningMove } from "./bot";
+import { getBlanks, invertPlayer, Field } from "../game";
+import { blockMove, randomMove, winningMove } from "./bot";
 
 // the medium bot plays a wiining move, if it can
 // or blocks the opponent from winning
 // or plays the center field if it can
 // otherwise it plays a random move
 export function mediumMove(board: Field[], own: Field): number {
-  return -1
+  let move = winningMove(board, own);
+
+  if(move !== -1){
+    return move;
+  }
+
+  move = blockMove(board, own);
+
+  if(move !== -1){
+    return move;
+  }
+
+  if (board[4] === Field.EMPTY) {
+    return 4;
+  }
+
+  do {
+    move = randomMove(9);
+  } while (board[move] !== Field.EMPTY);
+
+  return move;
 }
 
 // this bot just tries to block a win
